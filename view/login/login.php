8a4096dffa2c9d8cd40abc4fef85a67081cee637
Norbali <norqui2007@gmail.com>
2025-12-09 19:15:23 -0500

    login vista

---
+++ view/login/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Login – KaiAdmin</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+
+    <link rel="stylesheet" href="../../web/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../web/assets/css/kaiadmin.min.css">
+    <link rel="stylesheet" href="../../web/assets/css/fonts.min.css">
+    
+</head>
+
+<body class="login">
+    <div class="wrapper wrapper-login">
+        <div class="container container-login animated fadeIn">
+            <h3 class="text-center">Iniciar Sesión</h3>
+
+            <form>
+                <div class="form-group">
+                    <label class="placeholder"><b>Usuario</b></label>
+                    <input type="text" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label class="placeholder"><b>Contraseña</b></label>
+                    <input type="password" class="form-control">
+                </div>
+
+                <button class="btn btn-primary btn-block mt-3">Entrar</button>
+            </form>
+        </div>
+    </div>
+
+    <script src="../../web/assets/js/core/jquery-3.7.1.min.js"></script>
+    <script src="../../web/assets/js/core/popper.min.js"></script>
+    <script src="../../web/assets/js/core/bootstrap.min.js"></script>
+</body>
+</html>
