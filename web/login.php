f1966e1a74481e8cc7d909f61ac750978f7ea5c5
Norbali <norqui2007@gmail.com>
2025-12-15 09:06:25 -0500

    corregir inicio sesión

--- web/login.php
+++ web/login.php
@@ -1,3 +1,7 @@
+<?php 
+    include_once '../lib/helpers.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,7 +20,7 @@
         <div class="container container-login animated fadeIn">
             <h3 class="text-center">Iniciar Sesi&oacute;n</h3>
             <div class="text-center">
-                    <img src="assets/img/logoGEOSALUD.png" alt="navbar brand" class="navbar-brand m-2" height="120">
+                    <img src="assets/img/logoGEOSALUD2.png" alt="navbar brand" class="navbar-brand m-5" height="120">
             </div>
             <!--MENSAJE DE ERROR CON VARIABLE DE SESION-->
             <?php if (isset($_SESSION['error'])): ?>
@@ -29,24 +33,27 @@
                 </div>
             <?php endif; ?>
 
-            <form>
+            <form action="<?php echo getUrl("Acceso","Acceso","login", false, "ajax")?>" method="post">
                 <div class="form-group">
-                    <label><b>Usuario</b></label>
-                    <input type="text" class="form-control">
+                    <label class=""><b>Documento*</b></label>
+                    <input type="text" class="form-control" id="documento" name="documento" minlength="9" maxlength="10" required>
                 </div>
 
                 <div class="form-group">
-                    <label ><b>Contraseña</b></label>
-                    <input type="password" class="form-control">
+                    <label class=""><b>Contrase&ntilde;a*</b></label>
+                    <input type="password" class="form-control" id="contraseña" name="contraseña" minlength="8" maxlength="16" required>
                 </div>
 
-                <button class="btn btn-primary btn-block mt-3">Entrar</button>
-            </form>
+                <div class="text-center">
+                     <button type="submit" class="btn btn-primary" >Entrar</button>
+                </div>
+                </form>
         </div>
     </div>
 
     <script src="assets/js/core/jquery-3.7.1.min.js"></script>
     <script src="assets/js/core/popper.min.js"></script>
     <script src="assets/js/core/bootstrap.min.js"></script>
+    
 </body>
 </html>
