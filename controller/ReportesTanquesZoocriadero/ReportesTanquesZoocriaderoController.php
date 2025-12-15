d8b0d9dbb34543dc353785a1e9cbde5b99dc0690
Norbali <norqui2007@gmail.com>
2025-12-15 01:18:47 -0500

    Reporte de tanques por zoocriadero
    Falta aplicar bien los filtros

--- controller/ReportesTanquesZoocriadero/ReportesTanquesZoocriaderoController.php
+++ controller/ReportesTanquesZoocriadero/ReportesTanquesZoocriaderoController.php
@@ -32,7 +32,6 @@
                     ON tt.id_tipo_tanque = t.id_tipo_tanque
                 ORDER BY z.nombre_zoocriadero, t.id_tanque;
 
-
             ";
 
             return $obj->select($sql);
@@ -41,15 +40,31 @@
         public function filtro(){
             $obj = new ReportesTanquesZoocriaderoModel();
 
-            $id_zoocriadero = $_POST['zoocriadero'];
+            $buscar = $_GET['buscar'];
 
-            $cosultaTanquesZoocriadero = $this->cosultaTanquesZoocriadero($id_zoocriadero);
+            $sql = "
+                    SELECT
+                        z.id_zoocriadero,
+                        z.nombre_zoocriadero,
+                        ez.nombre_estado_zoocriaderos AS estado_zoocriadero,
+                        t.id_tanque,
+                        tt.nombre_tipo_tanque
+                    FROM zoocriadero z
+                    INNER JOIN estado_zoocriaderos ez
+                        ON ez.id_estado_zoocriaderos = z.id_estado_zoocriadero
+                    INNER JOIN tanque t
+                        ON t.id_zoocriadero = z.id_zoocriadero
+                    INNER JOIN tipo_tanque tt
+                        ON tt.id_tipo_tanque = t.id_tipo_tanque
+                    WHERE ('$buscar' = '' OR z.nombre_zoocriadero ILIKE '%$buscar%')
+                    ORDER BY z.nombre_zoocriadero, t.id_tanque
+                ";
 
-            $sqlZoocriaderos = "SELECT * FROM zoocriadero";
-            $zoocriaderos = $obj->select($sqlZoocriaderos);
+            $cosultaTanquesZoocriadero = $obj->select($sql);
 
-            include_once '../view/reportesTanquesZoocriadero/reporteTanquesZoocriadero.php';
+            include_once "../view/reportesTanquesZoocriadero/tanquesZoocriaderoFiltro.php";
         }
+
     }
 
 ?>
